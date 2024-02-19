2f269a9f3028facc43da2080837e1fb8ff73cee4
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-02-19 11:36:34 +0100

    add async data fetch

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,5 +1,7 @@
 "use strict";
 
+window.onload = loadAntagning();
+
 /* chart.js för att skapa diagram */
 const ctx = document.getElementById('myChart');
 
@@ -26,7 +28,15 @@ new Chart(ctx, {
 
 async function loadAntagning() {
     try {
+        /* hämta datan async */
         const response = await fetch(`https://studenter.miun.se/~mallar/dt211g/`);
         const data = await response.json();
+        /*  sortera ut endast kurserna */
+        const courseData = data.filter(element => element.type == 'Kurs');
+        /* sortera ut endast programmen */
+        const programData = data.filter(element => element.type == 'Program');
+
+    } catch (error) {
+        console.log(error);
     }
 }
