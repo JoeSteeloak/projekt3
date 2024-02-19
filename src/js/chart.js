756d6fe8a8aae655474c54659cec34b2ba540b7f
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-02-19 12:54:04 +0100

    style charts

--- src/js/chart.js
+++ src/js/chart.js
@@ -14,48 +14,69 @@ async function loadAntagning() {
         /*  sortera ut endast kurserna */
         const courseData = data.filter(e => e.type == 'Kurs');
         /* sortera efter antal sökandet*/
-        courseData.sort(function(a, b){
+        courseData.sort(function (a, b) {
             return b.applicantsTotal - a.applicantsTotal;
         });
         /* klipp ner till bara de 6 mest populära kurserna  */
-        const finalCourseData = courseData.slice(0,6);
+        const finalCourseData = courseData.slice(0, 6);
         console.log(finalCourseData);
         /* plocka ut namnen på kurserna */
         const courseLabels = finalCourseData.map(obj => obj.name);
         /* plocka ut värdena */
         const courseValues = finalCourseData.map(obj => obj.applicantsTotal);
 
-/* chart.js för att skapa diagram */
-const ctx = document.getElementById('myChart');
+        /* chart.js för att skapa stapeldiagram */
+        const ctx1 = document.getElementById('myChart1');
 
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: courseLabels,
-        datasets: [{
-            label: 'Antal sökande',
-            data: courseValues,
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        new Chart(ctx1, {
+            type: 'bar',
+            data: {
+                labels: courseLabels,
+                datasets: [{
+                    label: 'Antal sökande',
+                    data: courseValues,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
             }
-        }
-    }
-});
+        });
 
         /* sortera ut endast programmen */
         const programData = data.filter(element => element.type == 'Program');
         /* sortera efter antal sökandet*/
-        programData.sort(function(a, b){
+        programData.sort(function (a, b) {
             return b.applicantsTotal - a.applicantsTotal;
         });
         /* klipp ner till bara de 5 mest populära programen  */
-        const finalProgramData = programData.slice(0,5);
+        const finalProgramData = programData.slice(0, 5);
         console.log(finalProgramData);
+        /* plocka ut namnen på kurserna */
+        const programLabels = finalProgramData.map(obj => obj.name);
+        /* plocka ut värdena */
+        const programValues = finalProgramData.map(obj => obj.applicantsTotal);
+
+        /* chart.js för att skapa stapeldiagram */
+        const ctx2 = document.getElementById('myChart2');
+
+        new Chart(ctx2, {
+            type: 'doughnut',
+            data: {
+                labels: programLabels,
+                datasets: [{
+                    label: 'Antal sökande',
+                    data: programValues,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+            }
+        });
 
     } catch (error) {
         console.log(error);
